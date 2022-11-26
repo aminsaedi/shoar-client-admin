9ffac546d2c5244cb58bb3c44edee8687f0ffe1e
Amin Saedi <amin.di.sam@gmail.com>
2022-11-26 18:15:43 -0500

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { io, Socket } from "socket.io-client";
+
+interface ServerToClientEvents {
+  next: (message: string) => void;
+}
+interface ClientToServerEvents {
+  shoarFromAdmin: (shoar: string) => void;
+  ping: (cb: () => void) => void;
+}
+
+const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
+  "http://24.48.6.239:3000",
+  {
+    auth: {
+      token: "montrealAdmin",
+    },
+  }
+);
 
 function App() {
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [value, setValue] = useState("");
+  const [latency, setLatency] = useState(0);
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      setIsConnected(true);
+    });
+
+    socket.on("disconnect", () => {
+      setIsConnected(false);
+    });
+
+    const pinger = setInterval(() => {
+      const start = Date.now();
+
+      socket.emit("ping", () => {
+        const duration = Date.now() - start;
+        setLatency(duration);
+      });
+    }, 2000);
+
+    return () => {
+      socket.off("connect");
+      socket.off("disconnect");
+      clearInterval(pinger);
+    };
+  }, []);
+
+  const onSend: () => void = () => {
+    socket.emit("shoarFromAdmin", value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>
+          Is Connected: {String(isConnected)} -- Latency: {latency}
+        </h1>
       </header>
+      <main>
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button onClick={onSend}>Broadcast Shoar</button>
+      </main>
     </div>
   );
 }
